1e475ff9dc5e2dcca3e1345d869f7e5ab4c005e0
cottons-kr <taeyeong.kim@algorix.io>
2024-11-19 14:19:36 +0900

    🔧 Bump version to 0.1.0 and add new props interfaces for layout and error handling

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,4 +1,19 @@
+import React from 'react'
+
 export interface BaseProps {
   className?: string
   style?: React.CSSProperties
+}
+export interface ILayoutProps {
+  children: React.ReactNode
+}
+
+export interface IErrorPageProps {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+export interface IParams<P extends string | number | symbol = '', S extends string | number | symbol = ''> {
+  params: Promise<Record<P, string>>
+  searchParams: Promise<Record<S, string>>
 }
